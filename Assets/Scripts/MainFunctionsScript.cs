6f7afcbf0bded689bf1386c45a236c95409bb8c3
solvevlos <solvevlos@gmail.com>
2021-02-03 12:39:37 -0800

    Script with main Unity script Functions

---
+++ Assets/Scripts/MainFunctionsScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFunctionsScript {
+	
+	// Called on the start of game, even if script is not used the first frame
+	// called only once
+	public void Awake() {
+		
+		
+	}
+	
+	// Called the first frame of the game, called only once
+	public void Start() {
+		
+		
+	}
+	
+	// Called every frame of the game.
+	public void Update() {
+		
+		
+	}
+	
+	// Called every Physics2D frame I think
+	// This one relates more to the actual motion and movement in the game
+	// I believe
+	public void FixedUpdate() {
+		
+		
+	}
+	
+	
+}
